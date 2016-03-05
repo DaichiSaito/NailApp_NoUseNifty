0a8db819b92c84315f6bb42ebc1606ca998421aa
DaichiSaito <h1a_ble@yahoo.co.jp>
2016-03-05 12:53:59 +0900

    Initial Commit

---
+++ NailApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  NailApp
+//
+//  Created by DaichiSaito on 2016/03/05.
+//  Copyright © 2016年 DaichiSaito. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NailApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  NailApp
+//
+//  Created by DaichiSaito on 2016/03/05.
+//  Copyright © 2016年 DaichiSaito. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
